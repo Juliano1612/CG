c85212f429e921fc59037645f6538805a3db1133
Juliano <julianocezargyn@hotmail.com>
2017-06-29 19:41:25 -0300

    Cilada

--- desktop/src/com/Juliano1612/cadeira/desktop/DesktopLauncher.java
+++ desktop/src/com/Juliano1612/cadeira/desktop/DesktopLauncher.java
@@ -1,17 +1,26 @@
 package com.Juliano1612.cadeira.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.Juliano1612.cadeira.CiCADa;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
 
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "CiCADa";
-		config.useGL30 = true;
-		config.height = 800;
-		config.width = 1200;
-		new LwjglApplication(new CiCADa(), config);
+		//int MINWIDTH = 800;
+		//int MAXWIDTH = Gdx.graphics.g;
+		//int MINHEIGHT = 1000;
+		//int MAXHEIGHT = Gdx.graphics.getHeight();
+
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+		//config.setWindowSizeLimits(MINWIDTH, MINHEIGHT, MAXWIDTH,MAXHEIGHT);
+		config.setTitle("CiCADa");
+		config.setMaximized(true);
+		//config.height = 800;
+		//config.width = 1200;
+		new Lwjgl3Application(new CiCADa(), config);
 	}
 }
