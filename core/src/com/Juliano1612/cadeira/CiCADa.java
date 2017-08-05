5c4006e9858328632202a02283abe746011ac624
Juliano <julianocezargyn@hotmail.com>
2017-08-04 23:01:44 -0300

    vamola

--- core/src/com/Juliano1612/cadeira/CiCADa.java
+++ core/src/com/Juliano1612/cadeira/CiCADa.java
@@ -1,19 +1,20 @@
 package com.Juliano1612.cadeira;
 
 import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import java.util.StringTokenizer;
 
-public class CiCADa extends ApplicationAdapter implements ApplicationListener {
+public class CiCADa extends ApplicationAdapter implements ApplicationListener, InputProcessor {
 
     PerspectiveCamera camera;
     Stage stage;
@@ -35,6 +36,7 @@ public class CiCADa extends ApplicationAdapter implements ApplicationListener {
         font.setColor(0.44f, 0.44f, 0.44f, 1);
 
         batch = new SpriteBatch();
+        renderer = new ShapeRenderer();
 
         camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         camera.position.set(15f, 15f, 15f);
@@ -43,44 +45,30 @@ public class CiCADa extends ApplicationAdapter implements ApplicationListener {
         camera.far = 300f;
         camera.update();
 
-        System.out.println(camera.viewportWidth + "  " + camera.viewportHeight);
-
-
-        renderer = new ShapeRenderer();
-
-        /*TERMINAL*/
-        stage = new Stage(); //window is stage
-        stage.clear();
-        InputListener terminal = new InputListener() {
-            @Override
-            public boolean keyTyped(InputEvent event, char character) {
-                if (dTerminal) {
-                    switch (character) {
-                        case '\n':
-                            System.out.println("ENTER");
-                            break;
-                        case '\b':
-                            if ((text.length() - 1) >= 0)
-                                text = text.substring(0, text.length() - 1);
-                            break;
-                        default:
-                            text += character;
-                            break;
-                    }
-                    return true;
-                }
-                return false;
-            }
-        };
-
-
         processor = new CameraInputController(camera);
+        Gdx.input.setInputProcessor(new InputMultiplexer(processor, this));
+    }
 
-        Gdx.input.setInputProcessor(processor);
-        stage.addListener(terminal);
+    private float help = 0f;
+    private void drawDottedLine(ShapeRenderer shapeRenderer, float x1, float y1, float z1, float x2, float y2, float z2) {
+        float dotDist = 0.25f;
+        ShapeRenderer.ShapeType revive = null;
+        if (shapeRenderer.isDrawing()) {
+            revive = shapeRenderer.getCurrentType();
+            shapeRenderer.end();
+        }
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Point);
 
+        Vector3 vec3 = new Vector3(x2, y2, z2).sub(new Vector3(x1, y1, z1));
+        float length = vec3.len();
+        for (float i = help; i < length; i += dotDist + help) {
+            vec3.clamp(length - i, length - i);
+            shapeRenderer.point(x1 + vec3.x, y1 + vec3.y, z1 + vec3.z);
+        }
 
-        Gdx.input.setInputProcessor(stage); //stage is responsive
+        shapeRenderer.end();
+        if (revive != null)
+            shapeRenderer.begin(revive);
     }
 
     @Override
@@ -91,22 +79,44 @@ public class CiCADa extends ApplicationAdapter implements ApplicationListener {
         verifyInputs();
 
         processor.update();
-        stage.act();
-        stage.draw();
 
         renderer.setProjectionMatrix(camera.combined);
         renderer.begin(ShapeRenderer.ShapeType.Line);
 
-        if(showLines){
-            renderer.setColor(Color.RED);
-            renderer.line(0f, 0f, 0f, 0f, 0f, 999999f);
-            renderer.setColor(Color.GREEN);
-            renderer.line(0f, 0f, 0f, 0f, 999999f, 0f);
-            renderer.setColor(Color.BLUE);
-            renderer.line(0f, 0f, 0f, 999999f, 0f, 0f);
-            renderer.setColor(0.44f, 0.44f, 0.44f, 1);
+        if (showLines) {
+            float inc = 2.5f;
+            int max = 999;
+            help = 0f;
+
+            Color red = Color.RED.cpy();
+            red.a = 0f;
+            renderer.setColor(red);
+            for (float i = 0; i < max; i += inc) {
+                drawDottedLine(renderer, i, 0f, -max, i, 0f, max);
+                drawDottedLine(renderer, 0f, i, -max, 0f, i, max);
+            }
+            help += 0.1f;
+
+            Color green = Color.GREEN.cpy();
+            green.a = 0.01f;
+            renderer.setColor(green);
+            for (float i = 0; i < max; i += inc) {
+                drawDottedLine(renderer,  i, -max, 0f, i, max, 0f);
+                drawDottedLine(renderer,  0f, -max, i, 0f, max, i);
+            }
+            help += 0.1f;
+
+            Color blue = Color.BLUE.cpy();
+            blue.a = 0.01f;
+            renderer.setColor(blue);
+            for (float i = 0; i < max; i += inc) {
+                drawDottedLine(renderer, -max, i, 0f, max, i, 0f);
+                drawDottedLine(renderer, -max, 0f, i, max, 0f, i);
+            }
+
         }
 
+        renderer.setColor(Color.WHITE);
         renderer.box(cx, cy, cz, 8f, 8f, 8f);
 
         renderer.end();
@@ -135,7 +145,7 @@ public class CiCADa extends ApplicationAdapter implements ApplicationListener {
             System.out.println("oi");
             dTerminal = true;
             terminalCommand = "Vamos transladar\nDigite o ponto para translação: ";
-        }else if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER) && dTerminal){
+        } else if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER) && dTerminal) {
             dTerminal = false;
             System.out.println("PO!!! capturei o ponto! " + text);
             StringTokenizer strtok = new StringTokenizer(text.trim());
@@ -147,13 +157,67 @@ public class CiCADa extends ApplicationAdapter implements ApplicationListener {
 
         }
 
-        if(Gdx.input.isKeyJustPressed(Input.Keys.F12) && !showLines){
+        if (Gdx.input.isKeyJustPressed(Input.Keys.F12) && !showLines) {
             showLines = true;
-        }else if(Gdx.input.isKeyJustPressed(Input.Keys.F12) && showLines){
+        } else if (Gdx.input.isKeyJustPressed(Input.Keys.F12) && showLines) {
             showLines = false;
         }
 
 //        camera.update();
     }
 
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        if (dTerminal) {
+            switch (character) {
+                case '\n':
+                    System.out.println("ENTER");
+                    break;
+                case '\b':
+                    if ((text.length() - 1) >= 0)
+                        text = text.substring(0, text.length() - 1);
+                    break;
+                default:
+                    text += character;
+                    break;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
